893b0b9e4b8e5d954a22a81946f45608e0da42b9
Nothing15457 <148003443+Nothing15457@users.noreply.github.com>
2023-10-18 18:24:05 -0300

    Update CreditsState.hx

--- source/menus/CreditsState.hx
+++ source/menus/CreditsState.hx
@@ -121,6 +121,12 @@ class CreditsState extends MusicBeatState
 		bg.color = getCurrentBGColor();
 		intendedColor = bg.color;
 		changeSelection();
+
+		#if mobile
+   addVirtualPad(UP_DOWN, A_B);
+   addVirtualPadCamera(false);
+   #end
+
 		super.create();
 	}
 
